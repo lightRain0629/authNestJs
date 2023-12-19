df7328b1ff07f73edd3a656dd9dc6bffb365212e
lightRain0629 <umyt.nuryagdiyev@gmail.com>
2023-12-19 19:18:44 +0500

    adding user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '@prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly prismaService: PrismaService) { }
+
+    // TODO dont forget to hash pasword 
+    save(user: Partial<User>) {
+        return this.prismaService.user.create({
+            data: {
+                email: user.email,
+                password: user.password,
+                roles: ["USER"]
+            }
+        })
+    }
+
+    findOne(idOrEmail: string) {
+        return this.prismaService.user.findFirst({
+            where: {
+                OR: [
+                    {
+                        id: idOrEmail
+                    },
+                    {
+                        email: idOrEmail
+                    }
+                ]
+            }
+        })
+    }
+
+    delete(id: string) { 
+        return this.prismaService.user.delete({ where:{ id }});
+    }
+}
