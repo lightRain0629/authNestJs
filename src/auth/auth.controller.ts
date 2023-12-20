d08038d9760022b9342cf807b77f9e6a9df9b599
lightRain0629 <umyt.nuryagdiyev@gmail.com>
2023-12-20 20:50:53 +0500

    adding authService and authContoller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,57 @@
+import { BadRequestException, Body, Controller, Get, HttpStatus, Post, Res, UnauthorizedException } from '@nestjs/common';
+import { RegisterDto } from './dto';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
+import { Tokens } from './interfaces';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
+
+const REFRESH_TOKEN = 'refreshtoken';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService, private readonly configService: ConfigService) { }
+
+    @Post('register')
+    async register(@Body() dto: RegisterDto) {
+        const user = await this.authService.register(dto);
+        if (!user) {
+            throw new BadRequestException(`Cannot to register as user with data ${JSON.stringify(dto)}`);
+        }
+        return user;
+    }
+
+    @Post('login')
+    async login(@Body() dto: LoginDto, @Res() res: Response) {
+        const tokens = await this.authService.login(dto);
+        if (!tokens) {
+            throw new BadRequestException(`Cannot to login as user with data ${JSON.stringify(dto)}`);
+        }
+        this.setRefreshTokenToCookies(tokens, res);
+        // return { accessToken: tokens.accessToken };
+        return tokens;
+    }
+
+    // TODO dont forget to add refresh tokens
+
+    @Get('refresh')
+    refreshTokens() {
+
+    }
+
+    private setRefreshTokenToCookies(tokens: Tokens, res: Response) {
+        if (!tokens) {
+            throw new UnauthorizedException()
+        }
+        res.cookie(REFRESH_TOKEN, tokens.refreshToken, {
+            httpOnly: true,
+            sameSite: 'lax',
+            expires: new Date(tokens.refreshToken.exp),
+            secure: this.configService.get('NODE_ENV', 'development') === 'production',
+            path: '/',
+        });
+        res.send(HttpStatus.OK);
+    }
+
+
+}
---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,16 @@
+ 
+import { IsPasswordsMatchingConstraint } from "@common/src/decorators/is-password-matching-constraint-decorator";
+import { IsEmail, IsString, MinLength, Validate } from "class-validator";
+
+export class RegisterDto {
+    @IsEmail()
+    email: string;
+    @IsString()
+    @MinLength(6)
+    password: string;
+    @IsString()
+    @MinLength(6)
+    @Validate(IsPasswordsMatchingConstraint)
+    passwordRepeat: string;
+
+}
