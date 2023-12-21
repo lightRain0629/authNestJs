cd293b91f04ca38eca8060a7f88b65d30007328e
lightRain0629 <umyt.nuryagdiyev@gmail.com>
2023-12-21 20:10:21 +0500

    adding jwt strategy

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+// TODO DONT FORGET TO ADD INTERFACE JWT PAYLOAD
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
