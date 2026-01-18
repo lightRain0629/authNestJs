000d6be1f5dc502093c68f4ab59e59538cb82a11
Umyt Nuryagdiyev <umyt.nuryagdiyev@gmail.com>
2026-01-19 00:04:51 +0500

    f

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
-      callbackURL: configService.get('GOOGLE_CALLBACK_URL'),
+      callbackURL: `https://api.umyt.dev/api/auth/google/callback`,
       scope: ['email', 'profile'],
     });
   }
