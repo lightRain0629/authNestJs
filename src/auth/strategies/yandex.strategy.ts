d6359ec85c6852f1d7e85a920dcd4fd0729fe4ec
Umyt Nuryagdiyev <umyt.nuryagdiyev@gmail.com>
2026-01-18 22:55:01 +0500

    callback url adjusted

--- src/auth/strategies/yandex.strategy.ts
+++ src/auth/strategies/yandex.strategy.ts
@@ -9,7 +9,7 @@ export class YandexStrategy extends PassportStrategy(Strategy, 'yandex') {
     super({
       clientID: configService.get('YANDEX_APP_ID'),
       clientSecret: configService.get('YANDEX_APP_SECRET'),
-      callbackURL: 'http://localhost:3000/api/auth/yandex/callback',
+      callbackURL: `${configService.get('FRONTEND_URL')}/api/auth/yandex/callback`,
     });
   }
 
